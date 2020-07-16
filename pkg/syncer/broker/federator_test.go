d27bb142b9c97cdcbeb8408f665f57713557078d
Daniel Farrell <dfarrell@redhat.com>
2020-07-16 05:38:58 -0400

    Enable lll line length linting, fix errors
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- pkg/syncer/broker/federator_test.go
+++ pkg/syncer/broker/federator_test.go
@@ -194,7 +194,8 @@ func testDelete() {
 	})
 }
 
-func setupFederator(resource *corev1.Pod, initObjs []runtime.Object, localClusterID string) (federate.Federator, *fake.DynamicResourceClient) {
+func setupFederator(resource *corev1.Pod, initObjs []runtime.Object, localClusterID string) (federate.Federator,
+	*fake.DynamicResourceClient) {
 	dynClient := fake.NewDynamicClient(test.PrepInitialClientObjs(test.RemoteNamespace, localClusterID, initObjs...)...)
 	restMapper, gvr := test.GetRESTMapperAndGroupVersionResourceFor(resource)
 	f := broker.NewFederator(dynClient, restMapper, test.RemoteNamespace, localClusterID)
