5615a666eb74161973a5b0c1817dd6875f0e015f
Stephen Kitt <skitt@redhat.com>
2021-07-09 11:37:11 +0200

    Provide package descriptions
    Currently, our package descriptions are our license headers, because
    the header blocks aren’t separated from the package declarations.
    This patch separates them, and adds short package descriptions.
    
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- pkg/watcher/resource_watcher.go
+++ pkg/watcher/resource_watcher.go
@@ -15,6 +15,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
+// Package watcher implements a resource watcher, allowing change events on resources to be watched.
 package watcher
 
 import (
