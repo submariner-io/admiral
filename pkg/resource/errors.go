5e629042dd5eaa9ca95193773c5fdd08acd3498f
Automated Release <release@submariner.io>
2023-05-24 10:30:07 -0400

    Add IsNotFoundErr function
    When retrieving a resource, the following errors can occur:
    
    - apierrors.IsNotFound : if the resource doesn't exist
    - meta.IsNoMatchError : if the resource's Kind doesn't exist
    - discovery.ErrGroupDiscoveryFailed : if the resource's group
      doesn't exist.
    
    There are various places where we try to retrieve a resource
    where the CRD may not exist. Add a function to consolidate the
    above checks.
    
    Signed-off-by: Automated Release <release@submariner.io>

---
+++ pkg/resource/errors.go
@@ -0,0 +1,44 @@
+/*
+SPDX-License-Identifier: Apache-2.0
+
+Copyright Contributors to the Submariner project.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package resource
+
+import (
+	"errors"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/client-go/discovery"
+)
+
+func IsNotFoundErr(err error) bool {
+	if apierrors.IsNotFound(err) || meta.IsNoMatchError(err) {
+		return true
+	}
+
+	var errGDF *discovery.ErrGroupDiscoveryFailed
+	if !errors.As(err, &errGDF) {
+		return false
+	}
+
+	for _, err := range errGDF.Groups {
+		return IsNotFoundErr(err)
+	}
+
+	return false
+}
