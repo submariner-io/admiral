555d17c8f6210a6fa4c053fbb73c160eed45726a
Tom Pantelis <tompantelis@gmail.com>
2025-02-11 23:29:24 -0500

    Increase unit test coverage in the util package
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- pkg/util/helper_test.go
+++ pkg/util/helper_test.go
@@ -19,11 +19,18 @@ limitations under the License.
 package util_test
 
 import (
+	"crypto/x509"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/submariner-io/admiral/pkg/resource"
+	"github.com/submariner-io/admiral/pkg/syncer/test"
 	"github.com/submariner-io/admiral/pkg/util"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 )
 
 var _ = Describe("DeeplyEmpty", func() {
@@ -74,5 +81,131 @@ var _ = Describe("DeeplyEmpty", func() {
 	Specify("an empty Service status should return true", func() {
 		Expect(util.DeeplyEmpty(util.GetNestedField(resource.MustToUnstructuredUsingDefaultConverter(&corev1.Service{}),
 			util.StatusField).(map[string]interface{}))).To(BeTrue())
+	})
+})
+
+var _ = Describe("FindGroupVersionResource", func() {
+	Specify("should return the GVR if existing", func() {
+		restMapper := test.GetRESTMapperFor(&corev1.Pod{})
+
+		gvr, err := util.FindGroupVersionResource(resource.MustToUnstructured(&corev1.Pod{}), restMapper)
+		Expect(err).To(Succeed())
+		Expect(*gvr).To(Equal(corev1.SchemeGroupVersion.WithResource("pods")))
+	})
+
+	Specify("should return an error if non-existent", func() {
+		restMapper := test.GetRESTMapperFor(&corev1.Pod{})
+
+		_, err := util.FindGroupVersionResource(resource.MustToUnstructured(&corev1.Service{}), restMapper)
+		Expect(err).To(HaveOccurred())
+	})
+})
+
+var _ = Describe("ToUnstructuredResource", func() {
+	Specify("should return the Unstructured obj and GVR on success", func() {
+		restMapper := test.GetRESTMapperFor(&corev1.Pod{})
+
+		obj, gvr, err := util.ToUnstructuredResource(&corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test-pod",
+			},
+			Spec: corev1.PodSpec{
+				NodeName: "node",
+			},
+		}, restMapper)
+
+		Expect(err).To(Succeed())
+		Expect(*gvr).To(Equal(corev1.SchemeGroupVersion.WithResource("pods")))
+		Expect(obj.GetName()).To(Equal("test-pod"))
+
+		spec := &corev1.PodSpec{}
+		_ = runtime.DefaultUnstructuredConverter.FromUnstructured(util.GetSpec(obj).(map[string]interface{}), spec)
+		Expect(spec.NodeName).To(Equal("node"))
+	})
+
+	Specify("should return an error if GVR non-existent", func() {
+		restMapper := test.GetRESTMapperFor(&corev1.Service{})
+
+		_, _, err := util.ToUnstructuredResource(&corev1.Pod{}, restMapper)
+		Expect(err).To(HaveOccurred())
+	})
+})
+
+var _ = Describe("AddCertificateErrorHandler", func() {
+	unknownAuthorityError := x509.UnknownAuthorityError{Cert: &x509.Certificate{
+		Raw: []byte{1, 2, 3},
+	}}
+
+	certificateInvalidError := x509.CertificateInvalidError{Cert: &x509.Certificate{
+		Raw: []byte{4, 5, 6},
+	}}
+
+	var (
+		errorLogged chan error
+		fatalLogged chan error
+	)
+
+	BeforeEach(func() {
+		errorLogged = make(chan error, 20)
+		fatalLogged = make(chan error, 20)
+
+		util.ErrorHook = func(err error, _ string, _ ...interface{}) {
+			errorLogged <- err
+		}
+
+		util.FatalHook = func(err error, _ string, _ ...interface{}) {
+			fatalLogged <- err
+		}
+
+		savedErrorHandlers := utilruntime.ErrorHandlers
+		DeferCleanup(func() {
+			utilruntime.ErrorHandlers = savedErrorHandlers
+		})
+	})
+
+	testCertificateErrorHandler := func(fatal bool, logged chan error) {
+		util.AddCertificateErrorHandler(fatal)
+
+		utilruntime.HandleError(unknownAuthorityError)
+		Expect(logged).To(Receive())
+
+		utilruntime.HandleError(unknownAuthorityError)
+		Expect(logged).ToNot(Receive())
+
+		utilruntime.HandleError(certificateInvalidError)
+		Expect(logged).To(Receive())
+
+		utilruntime.HandleError(certificateInvalidError)
+		Expect(logged).ToNot(Receive())
+	}
+
+	Context("with non-fatal", func() {
+		It("should log an error message on first certificate error", func() {
+			testCertificateErrorHandler(false, errorLogged)
+		})
+	})
+
+	Context("with fatal", func() {
+		It("should log a fatal message on first certificate error", func() {
+			testCertificateErrorHandler(true, fatalLogged)
+		})
+	})
+})
+
+var _ = Describe("GetMetadata", func() {
+	Specify("should return the metadata map if existing", func() {
+		pod := &corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:   "test-pod",
+				Labels: map[string]string{"foo": "bar"},
+			},
+		}
+
+		objMeta := util.GetMetadata(resource.MustToUnstructured(pod))
+		Expect(objMeta).ToNot(BeNil())
+		Expect(objMeta).To(HaveKeyWithValue("name", pod.Name))
+		Expect(objMeta).To(HaveKeyWithValue("labels", map[string]interface{}{"foo": "bar"}))
+
+		Expect(util.GetMetadata(&unstructured.Unstructured{})).To(BeEmpty())
 	})
 })
