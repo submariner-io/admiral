c9493454fa5147e27569cf6036c66a6754535c25
Tom Pantelis <tompantelis@gmail.com>
2025-07-24 07:53:48 -0400

    Add IdentifyingLabels to the create-or-update Federator
    ...to handle updating resources that don't have a well-known
    name (eg created via GenerateName) and are identified by well-known
    labels.
    
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- pkg/federate/create_or_update_federator.go
+++ pkg/federate/create_or_update_federator.go
@@ -20,12 +20,16 @@ package federate
 
 import (
 	"context"
+	"fmt"
+	"slices"
 
 	"github.com/submariner-io/admiral/pkg/log"
 	"github.com/submariner-io/admiral/pkg/resource"
 	"github.com/submariner-io/admiral/pkg/util"
 	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/dynamic"
 )
@@ -35,19 +39,22 @@ type CreateOrUpdateOptions struct {
 	RestMapper         meta.RESTMapper
 	TargetNamespace    string
 	LocalClusterID     string
+	IdentifyingLabels  []string
 	KeepMetadataFields []string
 }
 
 type createOrUpdateFederator struct {
 	*baseFederator
-	localClusterID string
+	localClusterID    string
+	identifyingLabels []string
 }
 
 //nolint:gocritic // Ignore hugeParam
 func NewCreateOrUpdateFederator(options CreateOrUpdateOptions) FederatorExt {
 	return &createOrUpdateFederator{
-		baseFederator:  newBaseFederator(options.Client, options.RestMapper, options.TargetNamespace, options.KeepMetadataFields),
-		localClusterID: options.LocalClusterID,
+		baseFederator:     newBaseFederator(options.Client, options.RestMapper, options.TargetNamespace, options.KeepMetadataFields),
+		localClusterID:    options.LocalClusterID,
+		identifyingLabels: options.IdentifyingLabels,
 	}
 }
 
@@ -65,20 +72,75 @@ func (f *createOrUpdateFederator) Distribute(ctx context.Context, obj runtime.Ob
 
 	f.prepareResourceForSync(toDistribute)
 
-	result, err := util.CreateOrUpdate[*unstructured.Unstructured](ctx, resource.ForDynamic(resourceClient), toDistribute,
-		func(obj *unstructured.Unstructured) (*unstructured.Unstructured, error) {
-			return util.CopyImmutableMetadata(obj, toDistribute), nil
+	result, newObj, err := util.CreateOrUpdateWithOptions[*unstructured.Unstructured](ctx,
+		util.CreateOrUpdateOptions[*unstructured.Unstructured]{
+			Client:            resource.ForDynamic(resourceClient),
+			Obj:               toDistribute,
+			IdentifyingLabels: f.getIdentifyingLabels(toDistribute),
+			MutateOnUpdate: func(obj *unstructured.Unstructured) (*unstructured.Unstructured, error) {
+				return util.CopyImmutableMetadata(obj, toDistribute), nil
+			},
 		})
 
 	if f.eventLogName != "" {
 		if result == util.OperationResultCreated {
-			logger.Infof("%s: Created %s \"%s/%s\" ", f.eventLogName, toDistribute.GetKind(), toDistribute.GetNamespace(),
-				toDistribute.GetName())
+			logger.Infof("%s: Created %s \"%s/%s\" ", f.eventLogName, newObj.GetKind(), newObj.GetNamespace(),
+				newObj.GetName())
 		} else if result == util.OperationResultUpdated {
-			logger.Infof("%s: Updated %s \"%s/%s\" ", f.eventLogName, toDistribute.GetKind(), toDistribute.GetNamespace(),
-				toDistribute.GetName())
+			logger.Infof("%s: Updated %s \"%s/%s\" ", f.eventLogName, newObj.GetKind(), newObj.GetNamespace(),
+				newObj.GetName())
 		}
 	}
 
 	return err
+}
+
+func (f *createOrUpdateFederator) Delete(ctx context.Context, obj runtime.Object) error {
+	objMeta := resource.MustToMeta(obj)
+	if objMeta.GetName() == "" && len(f.identifyingLabels) > 0 {
+		toDelete, resourceClient, err := f.toUnstructured(obj)
+		if err != nil {
+			return err
+		}
+
+		identifyingLabels := f.getIdentifyingLabels(toDelete)
+
+		list, err := resourceClient.List(ctx, metav1.ListOptions{
+			LabelSelector: labels.SelectorFromSet(identifyingLabels).String(),
+		})
+		if err != nil {
+			return err //nolint:wrapcheck // No need to wrap
+		}
+
+		// Filter out resources whose GenerateName field doesn't match.
+		list.Items = slices.DeleteFunc(list.Items, func(u unstructured.Unstructured) bool {
+			return resource.MustToMeta(&u).GetGenerateName() != objMeta.GetGenerateName()
+		})
+
+		if len(list.Items) > 1 {
+			return fmt.Errorf("found %d resources with labels %#v, expected 1",
+				len(list.Items), identifyingLabels)
+		}
+
+		if len(list.Items) == 1 {
+			obj = obj.DeepCopyObject()
+			resource.MustToMeta(obj).SetName(list.Items[0].GetName())
+		}
+	}
+
+	return f.baseFederator.Delete(ctx, obj)
+}
+
+func (f *createOrUpdateFederator) getIdentifyingLabels(obj *unstructured.Unstructured) map[string]string {
+	if len(f.identifyingLabels) == 0 {
+		return nil
+	}
+
+	identifyingLabels := map[string]string{}
+
+	for _, l := range f.identifyingLabels {
+		identifyingLabels[l] = obj.GetLabels()[l]
+	}
+
+	return identifyingLabels
 }
