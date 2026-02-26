1775e775c30036313db028a0bf09777335b0e539
Paddy <padgupta@microsoft.com>
2026-02-26 09:19:51 +0000

    Replace slash with dot in ConfigMap keys for K8s compliance
    Signed-off-by: Paddy <padgupta@microsoft.com>

--- pkg/workqueue/config.go
+++ pkg/workqueue/config.go
@@ -79,5 +79,5 @@ func ConfigFromGlobal(keyPrefix string, defaultConfig *Config) *Config {
 }
 
 func ToConfigMapDataKey(prefix, name string) string {
-	return fmt.Sprintf("%s.workqueue/%s", prefix, name)
+	return fmt.Sprintf("%s.workqueue.%s", prefix, name)
 }
